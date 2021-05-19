eeff164864dfa997843aa6f00505cc9e58f1d42b
BrunoEstevamRJ <bruno.estevam@outlook.com.br>
2021-05-18 21:32:26 -0300

    controller e post

--- src/main/java/com/example/teste/controller/DemoController.java
+++ src/main/java/com/example/teste/controller/DemoController.java
@@ -4,6 +4,7 @@ import com.example.teste.model.Post;
 import com.example.teste.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,14 @@ public class DemoController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = demoService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @RequestMapping(value="/posts/{id}", method=RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = demoService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 
